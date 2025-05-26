1bffd8a8e9312641ca55b041f2f344bbef837636
Tong Yang <tongyang80@yahoo.com>
2025-05-25 20:03:36 -0500

    fix: setup client side routing.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState, useEffect } from 'react'
-import { Container } from '@chakra-ui/react'
-import axios from 'axios'
+import * as React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect
+} from 'react-router-dom'
+import Home from './components/layout/Home'
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: redirect('/pokex')
+  },
+  {
+    path: '/pokex',
+    element: <Home />
+  }
+])
 
 const App = () => {
-  const [msg, setMsg] = useState([])
-
-  useEffect(() => {
-    const getMock = async () => {
-      const { data } = await axios.get(`${import.meta.env.BASE_URL}/api/v1/types`)
-      setMsg(data)
-    }
-    getMock()
-  }, [])
-
   return (
-    <Container>
-      {msg.map((type) => {
-        return (
-          <p key={crypto.randomUUID()}>{type}</p>
-        )
-      })}
-    </Container>
+    <RouterProvider router={Router} future={{ v7_startTransition: true }} />
   )
 }
 
---
+++ client/src/components/layout/Home.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Pokédex</h1>
+      <p>Explore the world of Pokémon!</p>
+    </div>
+  )
+}
+
+export default Home
